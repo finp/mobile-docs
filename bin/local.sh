777bfabe6c949ada8e916fe4180420f6677d475f
Paul Wright <pwright@redhat.com>
2018-05-21 10:11:33 +0100

    add navigation and remote content

--- bin/local.sh
+++ bin/local.sh
@@ -3,6 +3,7 @@
 # curl https://raw.githubusercontent.com/finp/trello/master/modules/test/pages/test.adoc > modules/test/pages/rantora-test.adoc
 
 
+
 # create html
 
 antora $1
@@ -10,3 +11,9 @@ antora $1
 #fix html
 
 ./bin/fixDiv.sh
+
+
+# utils
+# touch files for antora
+#  grep -o :.*.adoc modules/ROOT/nav.adoc |sed 's/:/touch /'
+#
