be3e521f7d353af728a46d6cead7cd290a0c0ed6
Paul Wright <pwright@redhat.com>
2019-07-21 17:02:41 +0100

    update

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -2,9 +2,9 @@
 echo 'netlify-publish-adoc'
 node_modules/.bin/antora --html-url-extension-style=indexify --pull ../antora-site.yml
 
-# ../bin/publish-adoc.sh > build/site/ds-audit.adoc
+# ../bin/publish-adoc.sh > 
 
 echo 'node-publish-adoc'
 
-ls -alR ../
-node index.js ../modules/ROOT/pages/ds-audit.adoc > ds-audit.adoc
+
+node index.js ../modules/ROOT/pages/ds-audit.adoc > ../build/site/ds-audit.adoc
