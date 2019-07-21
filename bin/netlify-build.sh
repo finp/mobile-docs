12e80b43deae3f7a40671576e068d92431ef322e
Paul Wright <pwright@redhat.com>
2019-07-21 21:38:31 +0100

    end of js dev, looping over files

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -2,9 +2,15 @@
 echo 'netlify-publish-adoc'
 node_modules/.bin/antora --html-url-extension-style=indexify --pull ../antora-site.yml
 
-# ../bin/publish-adoc.sh > 
 
 echo 'node-publish-adoc'
 
+# publish each file, eg
+# node index.js ../modules/ROOT/pages/ds-audit.adoc > ../build/site/ds-audit.adoc
 
-node index.js ../modules/ROOT/pages/ds-audit.adoc > ../build/site/ds-audit.adoc
+for filename in ../modules/ROOT/pages/*.adoc; do
+
+  node index.js  $filename > ../build/site/$(basename "$filename")
+
+done
+
