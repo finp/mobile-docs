21de44096bac5d16336b37e76b802b5a0db2447b
Paul Wright <pwright@redhat.com>
2019-07-21 12:13:17 +0100

    set build dir

--- bin/netlify-build.sh
+++ bin/netlify-build.sh
@@ -1,4 +1,4 @@
 
 echo 'netlify-publish-adoc'
 node_modules/.bin/antora --html-url-extension-style=indexify --pull ../antora-site.yml
-./bin/publish-adoc > build/site/ds-audit.adoc
+./bin/publish-adoc > /opt/build/repo/build/site/ds-audit.adoc
